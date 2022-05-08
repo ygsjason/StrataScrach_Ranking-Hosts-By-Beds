553a44ccb405178ec426c8f934ea369b4d426238
Jason Yang <99283954+ygsjason@users.noreply.github.com>
2022-05-08 10:36:40 -0500

    Update Ranking-Hosts-By-Beds.sql

--- Ranking-Hosts-By-Beds.sql
+++ Ranking-Hosts-By-Beds.sql
@@ -2,6 +2,5 @@ WITH temp_cte AS
 (select host_id, sum(n_beds) as nbeds
 from airbnb_apartments
 group by host_id)
-
 select host_id, nbeds, dense_rank() over(order by nbeds desc) as rank
 from temp_cte
